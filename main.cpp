b14f20723f3789684d9a19c6de4eae09fd6b4d77
Chudy Andrej Oliver <xchudy03@merlin.fit.vutbr.cz>
2016-11-27 23:33:17 +0100

    ReMake waithers and generator guests

--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 #define KITCHEN_SLOTS 6
 #define WAITERS 2
 
-Histogram dobaVSystemu("Celkova doba v systemu", 0, 40, 20);
-Store restaurant("Store restaurant whit maximal capacity", CAPACITY);
-Store kitchen("Store restaurant whit maximal capacity", KITCHEN_SLOTS);
-Facility waiter("Waiter who receive orders");
-Facility waiter_2("Waiter who distributes food");
-Facility waiters[WAITERS];
+Histogram dobaVSystemu("Celkova doba v systemu", 0, 900, 16);
+Store restaurant("Store represented restaurant whit maximal capacity", CAPACITY);
+Store kitchen("Store represented parallel prepar food in kitchen", KITCHEN_SLOTS);
+Facility waiters[WAITERS].SetName("Waithers of restaurant");
 Queue waitForSoup;
 Queue waitForMainCourse;
+Queue orders;
 
-int i = 0;
-
+int counterLeavs = 0;
+int counterMainCourses = 0;
+int counterSoup = 0;
+int drinks = 0;
 
 
 class Food : public Process {
-
 public: 
 	bool manualyActivate;
 	bool isSoup;
@@ -31,15 +31,16 @@ public:
 	}
 
 	void Behavior() {
-		// printf("New foot\n");
+		
 		Enter(kitchen);
 		// printf("Enter\n");
 		if (isSoup) {
+			counterSoup++;
 			Wait(Uniform(100,200));
 		} else {
+			counterMainCourses++;
 			Wait(Uniform(400,500));
 		}
-
 		Leave(kitchen);
 		int who = -1;
 		if (waiters[1].Busy()) {
@@ -53,13 +54,11 @@ public:
 		}
 		if (who == -1) {
 			who = 1;
-			printf("Seize obsluha jedla na stol: %d\n", waiters[who].Q1->Length());
 			Seize(waiters[who], 1);
 		}
-	
 		if (!manualyActivate){
 			if (waiters[1].Q1->Length() > 0){
-				printf("Get 2 meal\n");
+				// printf("Get 2 meal\n");
 				Food * meal = (Food *)waiters[1].Q1->GetFirst();
 				meal->manualyActivate = true;
 				Wait(Uniform(20,25));
@@ -67,13 +66,30 @@ public:
 			} else {
 				Wait(Uniform(15,20));
 			}
+
+			
+
 			Release(waiters[who]);
 	 	}
-	 	if ( isSoup ){
-			waitForSoup.GetFirst()->Activate();
-	
-		} else {
-			waitForMainCourse.GetFirst()->Activate();
+	 	// Impatient food
+	 	while (true){
+		 	if ( isSoup ){
+		 		if (waitForSoup.Length() > 0){
+					waitForSoup.GetFirst()->Activate();
+					break;
+				} else {
+					Wait(10);
+					continue;
+				}
+			} else {
+				if (waitForMainCourse.Length() > 0){
+					waitForMainCourse.GetFirst()->Activate();
+					break;
+				} else {
+					Wait(10);
+					continue;
+				}
+			}
 		}
 	}
 };
@@ -81,41 +97,96 @@ public:
 class Order : public Process {
 public:
 	int whoWaiter;
+	bool mnaualyFree;
+	int foodCombo;
 
-	Order(int who) : Process() {
+	Order(int who, int foodCombination) : Process() {
 		whoWaiter = who;
+		mnaualyFree = false;
+		foodCombo = foodCombination;
 	}
 
 	void Behavior() {
 		//TODO may be interupted
-		// printf("New order\n");
-		printf("Before Seize get order: %d\n", waiters[whoWaiter].Q1->Length());
-		Seize(waiters[whoWaiter], 2);
-		Wait(Uniform(10,20));
-		(new Food(true))->Activate();
-		(new Food(false))->Activate();
-		Release(waiters[whoWaiter]);
-		printf("After Seize get order: %d\n", waiters[whoWaiter].Q1->Length());
+		Seize(waiters[whoWaiter], 1);
+		printf("New\n");
+		int drinksOrderd = 0;
+		if (!mnaualyFree){
+			int orderCounter = 1;
+			if (whoWaiter == 0 && waiters[0].Q1->Length() > 0){
+				printf("I have more orders\n");
+				for(int i = 0; i < (int) waiters[0].Q1->Length(); i++){
+					orderCounter++;
+					Order * ord = (Order *)waiters[0].Q1->GetLast();
+					ord->mnaualyFree = true;
+					ord->Activate();
+					if (Random() < 0.30)
+						drinksOrderd++;
+
+
+				}
+			}
+			if (!(foodCombo == 1)){
+				(new Food(true))->Activate();
+			}
+
+			if (!(foodCombo == 2)){
+				(new Food(false))->Activate();
+			}
+			//pripering drinks
+			if (Random() < 0.30)
+				drinksOrderd++;
+			if (drinksOrderd > 0){
+				drinks += drinksOrderd;
+				Wait(Uniform(60 * drinksOrderd, 90 * drinksOrderd));
+			}
+
+			printf("Waiting mulyiplay %d\n", orderCounter);
+			Wait(Uniform(4 * orderCounter, 6 * orderCounter));
+			Release(waiters[whoWaiter]);
+		} else {
+			if (!(foodCombo == 1)){
+				(new Food(true))->Activate();
+			}
+			
+			if (!(foodCombo == 2)){
+				(new Food(false))->Activate();
+			}
+		}
+		
+		// printf("After Seize get order: %d\n", waiters[whoWaiter].Q1->Length());
 	}
 };
+
+
+
 
 
 class Guest : public Process {
 	void Behavior() {
 		if( !restaurant.Full() ){
-			
+			int foodCombo = 0;
+			float randomNum = Random();
+			if (randomNum < 0.85){
+				foodCombo = 0;
+			} else if(randomNum < 0.98){
+				foodCombo = 1;
+			} else {
+				foodCombo = 2;
+			}
+
+
 			double tvstup = Time;
 			Enter(restaurant, 1);
 
-			// printf("Im here\n");
 			Wait(Uniform(15,40));	
 			Wait(Uniform(80,150));
+
 			int who = -1;
 			if (waiters[0].Busy()) {
 				for (int i = 1; i < WAITERS; i++) {
 					if (!waiters[i].Busy()) {
 						who = i;
-						printf("Zakakujem: %d   0\n", waiters[i].Q1->Length());
 						Seize(waiters[i]);
 						break;
 					}
@@ -123,26 +194,26 @@ class Guest : public Process {
 			}
 			if (who == -1) {
 				who = 0;
-				printf("Seize objednavanie: %d   0\n", waiters[0].Q1->Length());
 				Seize(waiters[0], 3);
 			}
 		
-			Wait(Uniform(20,30));	
-			(new Order(who))->Activate();	//make order
-			Release(waiters[who]);
-
-			// printf("Wait for soup ...\n");
-
-			Into(waitForSoup);
-			Passivate();
-			Wait(Uniform(100,150));		//eating soup
-
-			// printf("Wait for main course\n");
-			Into(waitForMainCourse);
-			Passivate();
-			Wait(Uniform(600,1000));		//eating main course
-
+			Wait(Uniform(15,20));	
 			
+			(new Order(who, foodCombo))->Activate();	//make order
+			
+
+
+			Release(waiters[who]);
+			if (!(foodCombo == 1)){
+				Into(waitForSoup);
+				Passivate();
+				Wait(Uniform(200,400));		//eating soup
+			}
+			if (!(foodCombo == 2)){
+				Into(waitForMainCourse);
+				Passivate();
+				Wait(Uniform(600,1200));		//eating main course
+			}
 			who = -1;
 			if (waiters[0].Busy()) {
 				for (int i = 1; i < WAITERS; i++) {
@@ -153,43 +224,174 @@ class Guest : public Process {
 					}
 				}
 			}
-			printf("Platenie Who %d    %d\n", who,waiters[0].Q1->Length() );
+			
 			if (who == -1) {
 				who = 0;
 				Seize(waiters[who], 3);
 			}
+			printf("Platenie Who %d    %d\n", who,waiters[0].Q1->Length() );
 			Wait(Uniform(20,60));	
 			Release(waiters[who]);
 
-			
-
-			printf("Thank you by\n");
+			// printf("Thank you by\n");
 			Leave(restaurant);
 			dobaVSystemu(Time - tvstup);
-		} else {
-			 
-			 i++;
+		} else { 
+			counterLeavs++;
 		}
-		// printf("Leave !!! %d\n", i);
+		 
 
 	}
 };
 
-
+/**
+ * Class to generate guests
+ */
 class Generator : public Event { 
     void Behavior() {
-        (new Guest)->Activate();
-        Activate(Time + Uniform(0,40)); 
+
+    	(new Guest)->Activate();
+    	if (Time < 1800){
+			Activate(Time + Uniform(0,200)); 
+    	} else if(Time < 3600) {
+    		Activate(Time + Uniform(0,110)); 
+		} else if(Time < 5400) {
+			Activate(Time + Uniform(0,80)); 
+		} else if(Time < 7200) {
+			Activate(Time + Uniform(0,90)); 
+		} else if(Time < 9000) {
+			Activate(Time + Uniform(0,100)); 
+		} else if(Time < 10800) {	
+			Activate(Time + Uniform(0,180)); 
+		} else if(Time < 12600) {
+			Activate(Time + Uniform(0,230)); 
+		} else {
+			Activate(Time + Uniform(0,400)); 
+		}
     }
 };
 
-
 int main(){
-	Init(0, 14000);
-	(new Generator)->Activate();
-	Run();
+	int numbOfExperiment = 0; 
+	scanf("%d",&numbOfExperiment);
+	//RandomSeed(Time(NULL));
 
+
+	switch(numbOfExperiment){
+		/* ---------------------------------------------- */
+		/*   			TEST 0						      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 0:
+			printf("0. Test starting ... ");
+			Init(0, 14400);
+			//
+			(new Generator)->Activate();
+			Run();
+
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 1				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 1:
+			printf("1. Experiment starting ... ");
+
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 2				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 2:
+			printf("2. Experiment starting ... ");
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 3				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 3:
+			printf("3. Experiment starting ... ");
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 4				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 4:
+			printf("4. Experiment starting ... ");
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 5 				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 5:
+			printf("5. Experiment starting ... ");	
+
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 6				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 6:
+			printf("6. Experiment starting ... ");
+
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 7				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 7:
+			printf("7. Experiment starting ... ");
+
+
+
+			break;
+		/* ---------------------------------------------- */
+		/*   			EXPERIMENT 8				      */
+		/* node  */
+		/* -----------------------------------------------*/
+		case 8:
+			printf("8. Experiment starting ... ");
+			break;
+
+	}
+
+	waiters[0].Output();
+	waiters[1].Output();
 	dobaVSystemu.Output();
+	kitchen.Output();
+	printf("Counter of prepared soups: %d\n", counterSoup);
+	printf("Counter of prepared main courses: %d\n", counterMainCourses);
+	printf("Counter of prepared drinks: %d\n", drinks);
+	printf("Leave people:  %d\n", counterLeavs);
+	printf("Zisk:  %d\n", (counterSoup * 15) + (counterMainCourses * 70) + drinks * 30);
+
+	
+	printf("e: %f\n", Exponential(100) );
+	printf("e: %f\n", Exponential(0.1) );
+	printf("e: %f\n", Exponential(1) );
+	printf("e: %f\n", Exponential(2) );
+
 }
 
 
